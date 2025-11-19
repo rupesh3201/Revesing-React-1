53d77562febd09c9c973cecf3163c3b6ae0a8bd7
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-11-19 22:49:59 +0530

    anatomy of react

--- FirstFlight/src/main.jsx
+++ FirstFlight/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>First Flight</h1>)
 
-    <App />
-,
-)
