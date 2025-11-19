a35bbf9471b66e7d3b9a90ad6028bc69de595225
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-11-19 23:22:26 +0530

    Component work sucess

--- component/src/main.jsx
+++ component/src/main.jsx
@@ -1,15 +1,14 @@
 
 import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById("root"));
-const num = 10
-root.render(<h1>First Flight {2+2}  {num % 2 == 0  ? "even" : "odd"}</h1>),
-function Greeating ( )
+function Greeating()
 {
-  return <h1>Hi, have a great day </h1>
+  return <h1>Hi, have a great day </h1>;
 
 }
-root.render(<div> 
-  <Greeating/>
-</div>)
 
+
+
+const root = createRoot(document.getElementById("root"));
+const num = 10
+root.render(<h1>First Flight {2+2} <Greeating/> {num % 2 == 0  ? "even" : "odd"}</h1>)
