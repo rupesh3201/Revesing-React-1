ca6a27663d4cfe344edef98d134013348fd2436e
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-03 22:48:30 +0530

    learning named expoert

--- component/src/main.jsx
+++ component/src/main.jsx
@@ -1,18 +1,26 @@
-
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById("root"));
-const num = 10
-function Greeating(props)
-{
-  return (<h1>Hi, have a great day {props.studentname}<h3></h3> </h1>);
+const num = 10;
+
+export function Greeting(props) {
+  return (
+    <div>
+      <h1>Hi, have a great day {props.studentname}</h1>
+      <h3></h3>
+    </div>
+  );
 }
+
 root.render(
   <div>
-<h1>First Flight {2+2}
-<Greeating studentname={"Rupesh"}/> 
-<Greeating studentname={"Panakj"}/> 
-<Greeating studentname={"Parnita"}/> 
-<Greeating studentname={"Tanuja"}/> 
-{num % 2 == 0  ? "even" : "odd"}</h1>
-</div>);
+    <h1>First Flight {2 + 2}</h1>
+
+    <Greeting studentname="Rupesh" />
+    <Greeting studentname="Pankaj" />
+    <Greeting studentname="Parnita" />
+    <Greeting studentname="Tanuja" />
+
+    {num % 2 === 0 ? "even" : "odd"}
+  </div>
+);
