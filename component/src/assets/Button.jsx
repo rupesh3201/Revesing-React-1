359e36994bb12a693cfb435a8d66b7da3e459431
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-03 22:54:02 +0530

    created naamed expoet

---
+++ component/src/assets/Button.jsx
@@ -0,0 +1,10 @@
+function Hibutton(){
+    return <button>Hii</button>;
+}
+function Hellobutton(){
+    return <button>Hello</button>;
+} 
+function Byebutton(){
+    return <button>Bye</button>;
+}
+export {Hibutton, Hellobutton , Byebutton};
--- named_default_export/vite_project/src/main.jsx
+++ named_default_export/vite_project/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+const num = 10;
+
+export function Greeting(props) {
+  return (
+    <div>
+      <h1>Hi, have a great day {props.studentname}</h1>
+      <h3></h3>
+    </div>
+  );
+}
+
+root.render(
+  <div>
+    <h1>First Flight {2 + 2}</h1>
+
+    <Greeting studentname="Rupesh" />
+    <Greeting studentname="Pankaj" />
+    <Greeting studentname="Parnita" />
+    <Greeting studentname="Tanuja" />
+
+    {num % 2 === 0 ? "even" : "odd"}
+  </div>
+);
