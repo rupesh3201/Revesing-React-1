6fec6575491fa08a4bada054066df13bec83e1bd
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-10 22:10:18 +0530

    small white space diff matter

--- Routing_in_react/vite-project/src/main.jsx
+++ Routing_in_react/vite-project/src/main.jsx
@@ -1,14 +1,17 @@
 
 import { createRoot } from 'react-dom/client'
-
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Contact from './pages/Contact'
 const root = createRoot(document.getElementById("root"))
 const path = window.location.pathname;
-root.render(
-<h1>path: {path}</h1>,
- <h1><Home/></h1>,
-  <h1><About/></h1>,
-  <h1><Contact/></h1>,
-)
+
+if (path === "/"){
+  root.render(<h2>Home</h2>)
+}
+// root.render(
+// <h1>path: {path}</h1>,
+//  <h1><Home/></h1>,
+//   <h1><About/></h1>,
+//   <h1><Contact/></h1>,
+// )
