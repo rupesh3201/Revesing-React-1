a68aed9e14ba0a555afd92d11daf022259f9ba4a
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-10 21:57:52 +0530

    reder about

--- Routing_in_react/vite-project/src/main.jsx
+++ Routing_in_react/vite-project/src/main.jsx
@@ -2,9 +2,12 @@
 import { createRoot } from 'react-dom/client'
 
 import Home from './pages/Home'
-
+import About from './pages/About'
+import Contact from './pages/Contact'
 const root = createRoot(document.getElementById("root"))
 
 root.render(
  <h1><Home/></h1>,
+  <h1><About/></h1>,
+  <h1><Contact/></h1>,
 )
