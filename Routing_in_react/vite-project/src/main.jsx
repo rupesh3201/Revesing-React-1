9eb268c400b9c4ed8787cb82d0d064f06aa55b74
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-10 22:03:30 +0530

    showing path

--- Routing_in_react/vite-project/src/main.jsx
+++ Routing_in_react/vite-project/src/main.jsx
@@ -5,8 +5,9 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 const root = createRoot(document.getElementById("root"))
-
+const path = window.location.pathname;
 root.render(
+<h1>path: {path}</h1>,
  <h1><Home/></h1>,
   <h1><About/></h1>,
   <h1><Contact/></h1>,
