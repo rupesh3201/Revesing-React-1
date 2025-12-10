ada8c91c7440b98c87437bf3613b4b7e8a388454
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-12-10 22:14:24 +0530

    Done Setup

--- Routing_in_react/vite-project/src/main.jsx
+++ Routing_in_react/vite-project/src/main.jsx
@@ -7,14 +7,17 @@ const root = createRoot(document.getElementById("root"))
 const path = window.location.pathname;
 console.log("current path",path)
 if (path === "/"){
-  root.render(<h2> i am on Home</h2>)
+  root.render(<h2> i am on Home</h2>);
 }
 else if (path === "/about"){
- root.render(<h2> i am on about page</h2>)
+ root.render(<h2> i am on about page</h2>);
 }
 else if (path === "/contact"){
- root.render(<h2> i am on contact page</h2>)
-
+ root.render(<h2> i am on contact page</h2>); 
+}
+else{
+  root.render(<h2> 404 not found</h2>);
+}
 // root.render(
 // <h1>path: {path}</h1>,
 //  <h1><Home/></h1>,
