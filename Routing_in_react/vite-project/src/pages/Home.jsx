ec185d0895b8c61ff3581b71390131a051889cb8
rupesh3201 <rupesh.mandge3201@gmail.com>
2026-02-03 21:13:22 +0530

    created basic addition  bil

--- Routing_in_react/vite-project/src/pages/Home.jsx
+++ Routing_in_react/vite-project/src/pages/Home.jsx
@@ -4,26 +4,26 @@ import Button from '../component/Button'
 
 function Home() {
 
-  
-  
+  const price = 500 ;
   const  [ count , setCount] = useState (0);
   return (
     <>
    
      <Nav activeMenu = "/" />
         <Button title= "Login" variant="login" />
-         &nsbp;<h1>Value : {count}</h1>
+         &nbsp; <h1>Quantity: {count}</h1> &nbsp; <h1> price : {price}</h1>
          <button onClick={()=>{
         setCount( count + 1);
           console.log(count);
          }}>Increase</button> 
 
-        
+         &nbsp; 
           <button onClick={()=>{
             setCount( count - 1);
           console.log(count);
           }}>Decrease</button>
-
+          
+          <h1> Total bill is  {price *count}</h1>
 
       <div>content of Home</div>
     </>
